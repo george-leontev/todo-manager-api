16d62b2fe1bda14bf29a69268e9b02da7fff8c43
George <egorleontev54@gmail.com>
2023-12-23 16:48:38 +0300

    json

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ async def get():
 def get_data():
     root = pathlib.Path(__file__).parent.parent
 
-    with open(f'{root}\\data\\data.json', 'r', encoding='utf-8') as f:
+    with open(f'{root}/data/data.json', 'r', encoding='utf-8') as f:
         data = f.read()
 
     return Response(content=data, media_type="application/json")
