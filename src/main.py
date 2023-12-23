261478b648bcb4944d76d24d394adb4e8b28477f
George <egorleontev54@gmail.com>
2023-12-23 16:42:48 +0300

    json

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+import pathlib
 
 app = FastAPI()
 
 @app.get('/')
 async def get():
     return {'message': 'Hello world'}
+
+@app.get('/data')
+def get_data():
+    root = pathlib.Path(__file__).parent.parent
+
+    with open(f'{root}\\data\\data.json', 'r', encoding='utf-8') as f:
+        data = f.read()
+
+    return Response(content=data, media_type="application/json")
