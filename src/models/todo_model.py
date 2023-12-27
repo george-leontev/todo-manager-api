357f2329568b87e52c9e0de1cded7765f6c7ff15
George <egorleontev54@gmail.com>
2023-12-27 18:28:08 +0300

    fix: models

--- src/models/todo_model.py
+++ src/models/todo_model.py
@@ -1,9 +1,15 @@
 from datetime import datetime
+from enum import IntEnum
 
 from pydantic import BaseModel
+
+class TodoStatus(IntEnum):
+    PENDING = 1,
+    DONE = 2
+
 
 class TodoModel(BaseModel):
     id: int
     description: str
     date: datetime
-    status: str
+    status: TodoStatus
